f37a8164ddd42b1e0f79e480d0b8b71beaf116f4
unknown <sulastrievi82@gmail.com>
2020-12-17 16:43:05 +0800

    laravel8

---
+++ resources/views/berita/table.blade.php
@@ -0,0 +1,22 @@
+<table class="table">
+    <thead calss="thead-dark">
+        <tr style="background-color:#31b0d5">
+            <th>no</th>
+            <th>judul</th>
+            <th>isi</th>
+            <th>aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($beritas as $item)
+            <tr>
+              <td>{{ $loop->iteration }} </td>
+              <td>{{ $item->judul }}</td>
+              <td>{{ $item->isi }}</td>
+              <td>
+                    <a href="{{ route('berita.show', $item->id) }}" class="btn btn-success">Tampil</a>
+              </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
